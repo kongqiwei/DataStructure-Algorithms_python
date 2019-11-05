28a5996aa0e154d5c8de29c319ee3d715826b9f9
七威 <603200245@qq.com>
2019-11-05 21:00:38 +0800

    Update SequenceStack.c

--- SequenceStack.c
+++ SequenceStack.c
@@ -133,3 +133,340 @@ void Print(ELemtype_Sq e)
 {
 	printf("%c",e);
 }
+//迷宫操作
+#include<stdio.h>
+#include<stdlib.h>
+#include<ctime.h>
+#define N  15
+#define X  4
+#define SleepTime 2
+typedef enum //枚举类型
+{
+	Wall,
+	Obstacle,
+	Way,
+	DeadLock,
+	East,
+	South,
+	West,
+	North
+}MazeNode;
+typedef struct 
+{
+	int x;
+	int y;
+}PosType;
+typedef struct
+{
+	int ord;
+	PosType seat;
+	int di;
+}ElemType_Sq;
+typedef int Mazetype;
+typedef int Status;
+Status  MazePath(Mazetype maze[][N],PosType start,PosType end)
+{
+	SqStack S;
+	ElemType_Sq nodeInf;
+	PosType curPos;
+	int curStep;
+	InitStack_Sq(&S);
+	curPos=start;
+	curStep=1;
+	do
+	{
+		if(Pass(curPos,maze))
+		{
+			FootPrint(curPos,maze);
+			ShowMaze(maze);
+			SetElemType(&nodeInf,curStep,curPos,East);
+			Push_Sq(&S,nodeInf);
+			if(EqualPosType(curPos,end))
+			{
+				printf("寻路成功\n");
+				return TRUE;
+			}
+			curPos=NextPos(curPos,East);
+			curStep++;
+		}
+		else
+		{
+			if(!StackEmpty_Sq(S))
+			{
+				Pop_Sq(&S,&nodeInf);
+				while(nodeInf.di==North&&!StackEmpty_Sq(S))
+				{
+					MarkPrint(nodeInf.seat,maze);
+					ShowMaze(maze);
+					Pop_Sq(&S,&nodeInf);
+				}
+				if(nodeInf.di<North)
+				{
+					maze[nodeInf.seat.x][nodeInf.seat.y]=++nodeInf.di;
+					ShowMaze(maze);
+					Push_Sq(&S,nodeInf);
+					curPos=NextPos(nodeInf.seat,nodeInf.di);
+				}
+			}
+		}
+	}while(!StackEmpty_Sq(S));
+	printf("寻路失败\n");
+	return FALSE;
+}
+void InitMaze(MazeType maze[][N],PosType *start,PosType *end)
+{
+	int i,j,tmp;
+	srand((unsigned)time(NULL));
+	for(i=0;i<N;i++)
+	{
+		for(j=0;j<N;j++)
+		{
+			if(i==0||j==0||i==N-1||j==N-1)
+				maze[i][j]=Wall;
+			else
+			{
+				tmp=rand()%X;
+				if(tmp)
+					maze[i][j]=Obstacle;
+				else
+					maze[i][j]=Way;
+			}
+		}
+	}
+	(*start).x=1;
+	(*start).y=0;
+	(*end).x=N-2;
+	(*end).y=N-1;
+	maze[1][0]=maze[N-2][N-1]=Way;
+	maze[1][1]=maze[N-2][N-2]=Way;
+}
+void PaintMaze(Mazetype maze[][N])
+{
+	int i,j;
+	for(i=0;i<N;i++)
+		for(j=0;j<N;j++)
+		{
+			if(maze[i][j]==Wall)
+				printf("fucked\n");
+			else if(maze[i][j]==Obstacle)
+				printf("fukced\n");
+			else if(maze[i][j]==East)
+				printf(">\n");
+			else fi (maze[i][j]==South)
+				printf("'\n",);
+			else if (maze[i][j]==West)
+				printf("<\n" );
+			else  if (maze[i][j]==North)
+				printf("^\n");
+			else
+				printf("  \n");
+			if(j!=0&&j%(N-1)==0)
+				printf("n\n");
+		}
+}
+void ShowMaze(MazeType maze[][N])
+{
+	Wall(SleepTime);
+	system("cls");
+	PaintMaze(maze);
+}
+Status EqualPosType(PosType seat1,PosType seat2)
+{
+	if(seat1.x==seat2.x&&seat1.y==seat2.y)
+		return TRUE;
+	else
+		return FALSE;
+}
+Status Pass(PosType seat,MazeType maze[][N])
+{
+	int x=seat.x;
+	int y=seat.y;
+	if(!IsCross(seat)&&maze[x][y]==Way)
+		return TRUE;
+	else
+		return FALSE;
+}
+void  FootPrint(PosType seat,MazeType maze[][N])
+{
+	maze[seat.x][seat.y]=East;
+}
+void SetElemType(ElemType_Sq *e,int ord,PosType seat,int di)
+{
+	(*e).ord=ord;
+	(*e).seat=seat;
+	(*e).di=di;
+}
+PosType NextPos(PosType seat,int di)
+{
+	PosType tmp=seat;
+	switch(di)
+	{
+		case East:tmp.y++;break;
+		case West:tmp.y--;break;
+		case South:tmp.x++;break;
+		case North:tmp.x--;break;
+	}
+	return tmp;
+}
+Status IsCross(PosType seat)
+{
+	int x=seat.x;
+	int y=seat.y;
+	if(x<0||y<0||x>N-1||y>N-1)
+		return TRUE;
+	else
+		return FALSE;
+}
+void MarkPrint(PosType seat,MazeType maze[][N])
+{
+	int x=seat.x;
+	int y=seat.y;
+	maze[x][y]=koDeadLock;
+}
+
+//表达式求值
+typedef char ElemType_Sq;
+typedef ElemType_Sq OperandType;
+typedef ELemtype_Sq OperatorType
+OperandType EvaluateExpression(char exp[])
+{
+	SqStack OPTR,OPND;
+	ElemType_Sq e,ch;
+	OperatorType theta,x;
+	OperandType a,b;
+	int i;
+	InitStack_Sq(&OPTR);
+	Push_Sq(&OPTR,"#");
+	InitStack_Sq(OPND);
+	i=0;
+	ch=exp[i++];
+	while(ch!='#'||GetTop_OPTR(OPTR)!='#')
+	{
+		if(!IN(ch))
+		{
+			Push_Sq(&OPND,ch);
+			ch=exp[i++];
+		}
+		else
+		{
+			switch(Precede(GetTop_OPTR(OPTR),ch))
+			{
+				case '<':
+				   Push_Sq(&OPTR,ch);
+				   ch=exp[i++];
+				   break;
+				case '=':
+				   Pop_Sq(&OPTR,&x);
+				   ch=exp[i++];
+				   break;
+				case '>':
+				    Pop_Sq(&OPTR,&theta);
+				    Pop_Sq(&OPND,&b);
+				    Pop_Sq(&OPND,&a);
+				    break;
+			}
+		}
+	}
+	return GetTop_OPTR(OPND);
+}
+OperatorType GetTop_OPTR(SqStack OPTR)
+{
+	ElemType_Sq e;
+	GetTop_Sq(OPTR,&e);
+	return e;
+}
+Status In(ElemType_Sq c)
+{
+	switch(c)
+	{
+		case '+':
+		case '-':
+		case '*':
+		case '/':
+		case '(':
+		case ')':
+		case '#':
+		    return TRUE;
+		default:
+		    return FALSE;
+	}
+}
+OperatorType Precede(OperatorType o1,OperatorType o2)
+{
+	OperatorType f;
+	switch (o2)；
+	{
+		case'+':
+		case'-':
+		  if(o1=='('||o1=='#')
+			 f='<';
+		  else
+			 f='>';
+		  break;
+		case '*':
+		case '/':
+		   if(o1=='*'||o1=='/'||o1==')')
+		   	    f='>';
+		   	else
+		   		f='<';
+		   	    break;
+		case '(':
+			if(o1==')')
+			{
+				printf("括号匹配错误\n");
+				exit(ERROR);
+			}
+			else
+				f='<';
+			break;
+		case')':
+		    switch(o1)
+		    {
+		    	case'(':
+		    	     f='=';
+		    	     break;
+		    	case '#':
+		    	      printf("输入错误\n");
+		    	    exit(ERROR);
+		    	 default:
+		    	     f='>';
+		    }
+		    break;
+
+		 case '#':
+		    switch(o1)
+		    {
+		    	case'#':
+		    	   f='=';
+		    	   break;
+		    	case '(':
+		    	   printf("输入错误\n");
+		    	   break;
+		    	 default:
+		    	    f='>';
+		    }
+	}
+	return f;
+}
+OperandType(operate(OperandType a,OperatorType theta,OperandType b)
+{
+	int x,y,z;
+	x=a-48
+	y=b-48;
+	switch(theta)
+	{
+		case '+':
+		    return x+y+48;
+		    break;
+		case '-':
+		     return x-y+48;
+		     break;
+		case '*':
+		     return x*y+48;
+		     break;
+		case '/':
+		     return x/y+48;
+		     break;
+	}
+}
+
